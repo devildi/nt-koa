ef5cfb839efcc5936a525421584af0a84c1b0361
DevilDI <DevilDI@appledeMacBook-Pro.local>
2017-08-17 10:29:48 +0800

    8-17-10:30

--- views/js/map.js
+++ views/js/map.js
@@ -1,4 +1,5 @@
 var map
+var myloc = null
 var pos
 var userData
 var isiPhone = navigator.userAgent.toLocaleLowerCase().match(/iPhone/i)
@@ -10,115 +11,6 @@ var dinner = []
 var hotel = []
 var timer
 var continuousPositioning = false
-var userdata = {
-    play: [
-    [{
-      nameOfScene:'巴黎圣母院',
-      des:'寻觅钟楼怪人，巴黎最古老的天主教堂/8欧/10:30-18:30/1h',
-      location:{lat: 48.85291970657539, lng: 2.349676787853241}
-    },
-    {
-      nameOfScene:'卢浮宫',
-      des:'世界三大博物馆之一，《断臂维纳斯》《胜利女神像》《蒙娜丽莎》三大镇宅之宝！/12欧',
-      location:{lat: 48.86067750938799, lng: 2.334122657775879}
-    },
-    {
-      nameOfScene:'杜乐丽花园',
-      des:'连接卢浮宫和协和广场，宜小做休整！',
-      location:{lat: 48.863020028820706, lng: 2.326744042358415}
-    },
-    {
-      nameOfScene:'协和广场',
-      des:'拿破仑从埃及运回高高的方尖碑矗立于此',
-      location:{lat: 48.86498, lng: 2.3218500000000404}
-    },
-    {
-      nameOfScene: '香榭丽舍大道',
-      des: '即使不买买买，依然值得逛逛逛！',
-      location:{lat: 48.86945833854196, lng: 2.3076782151222286}
-    },
-    {
-      nameOfScene: '雄狮凯旋门',
-      des: '大巴黎的地标，必去无疑！',
-      location:{lat: 48.87374758, lng: 2.29484351}
-    },
-    {
-      nameOfScene: '夏乐宫',
-      des: '观看埃菲尔铁塔的最佳地点！',
-      location:{lat: 48.8619569, lng: 2.2887075}
-    },
-    {
-      nameOfScene: '埃菲尔铁塔',
-      des: '记得网上订票哦！',
-      location:{lat: 48.8583701, lng: 2.2944813}
-    }],
-    [{
-      nameOfScene: '巴士底广场',
-      des: '巴黎的中山广场，只是离雨果故居比较近而已！',
-      location:{lat: 48.85325491, lng: 2.36941392}
-    },
-    {
-      nameOfScene: '蓬皮杜现代当代艺术中心',
-      des: '一个充满奇怪建筑的地方！',
-      location:{lat: 48.8606455, lng: 2.3500563}
-    },
-    {
-      nameOfScene: '新桥',
-      des: '塞纳河上最古老的桥！',
-      location:{lat: 48.8570535, lng: 2.3391365}
-    },
-    {
-      nameOfScene: '艺术桥',
-      des: '爱情锁啊爱情锁！！',
-      location:{lat: 48.8583459, lng: 2.3353197}
-    },
-    {
-      nameOfScene: '圣日耳曼德佩教堂',
-      des: '新庭信步拉丁区！',
-      location:{lat: 48.8539638, lng: 2.3321619}
-    },
-    {
-      nameOfScene: '蒙马特高地',
-      des: '最市井的巴黎就在此处！别忘了爱墙和圣心堂哦！！',
-      location:{lat: 48.8861947, lng: 2.3419952}
-    },
-    {
-      nameOfScene: '爱墙',
-      des: '各种各样的“我爱你”',
-      location:{lat: 48.8846336, lng: 2.3386048}
-    }
-    ],
-    [{
-      nameOfScene: '凡尔赛宫',
-      des: '气势恢宏，富丽堂皇，不得不去！',
-      location:{lat: 48.8048684, lng: 2.1181667}
-    }]
-    ],
-    dinner: [
-    {
-      nameOfScene: '小馋猫',
-      des: '招牌菜：红酒烩鸡,红酒烩猪头',
-      location:{lat: 41.773828, lng: 123.426843}
-    },
-    {
-      nameOfScene: '大馋猫',
-      des: '招牌菜：红酒烩猪头,红酒烩猪头',
-      location:{lat: 41.824486, lng: 123.490939}
-    }
-    ],
-    hotel:[
-    {
-      nameOfScene: 'clarrse',
-      des: 'checkin:10AM;checkout:2PM',
-      location:{lat: 41.77329, lng: 123.393641}
-    },
-    {
-      nameOfScene: '旅店',
-      des: 'checkin:10AM;checkout:255555PM',
-      location:{lat: 41.82368, lng: 123.41247}
-    }
-    ]
-}
 var playData = function(arr){
       let newarray = []
       for (var i = 0; i < arr.length; i++){
@@ -609,6 +501,8 @@ function initial(arr){
   //定位
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(complete, error)
+  } else {
+    alert('不支持定位！')
   }
   function complete(position) {
     pos = null
@@ -616,9 +510,12 @@ function initial(arr){
       lat: position.coords.latitude,
       lng: position.coords.longitude
     }
-    console.log(pos)
     map.setCenter(pos)
-    var myloc = new google.maps.Marker({
+    if(myloc){
+      myloc.setMap(null)
+      myloc = null
+    }
+    myloc = new google.maps.Marker({
       clickable: false,
       icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
                                                       new google.maps.Size(22,22),
@@ -641,7 +538,6 @@ function initial(arr){
       closeOnConfirm: false
     },
     function(){
-      swal("Deleted!", "Your imaginary file has been deleted.", "success")
       history.go(0) 
     })
   }
@@ -667,5 +563,4 @@ function initial(arr){
   $('.panel-title').delegate('a', 'click', function(e) {
     console.log(e.target.innerHTML)
   })
-
 }
